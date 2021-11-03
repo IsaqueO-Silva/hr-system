56a5150bea03a23fdea14e8f05b6b693ecdcb086
Isaque O. Silva <Isaque1945@hotmail.com>
2021-11-03 11:42:27 -0300

    Added some templates to the project

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
-echo 'teste';
+require_once('vendor/autoload.php');
+
+use \Slim\Slim;
+use \Isaque\Page;
+
+$app = new Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+
+    $page = new Page();
+
+    $page->setTpl('index');
+});
+
+$app->run();
 ?>
