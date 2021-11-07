2b7058b4b75d6d84ce8fcc8ab7c176dd13602100
Isaque O. Silva <Isaque1945@hotmail.com>
2021-11-06 22:42:43 -0300

    regions.php

---
+++ regions.php
@@ -0,0 +1,90 @@
+<?php
+
+use \Isaque\Page;
+use \Isaque\Model\User;
+use \Isaque\Model\Region;
+
+$app->get('/regions', function() {
+
+    User::verifyLogin();
+
+    $page = new Page();
+
+    $page->setTpl('regions', array(
+        'regions'   => Region::listAll(),
+        'msgError'  => Region::getError()
+    ));
+});
+
+$app->get('/regions/create', function() {
+
+    User::verifyLogin();
+
+    $page = new Page();
+
+    $page->setTpl('regions-create', array(
+        'msgError' => Region::getError()
+    ));
+});
+
+$app->get('/regions/:region_id/delete', function($region_id) {
+
+    User::verifyLogin();
+
+    $region = new Region();
+
+    $region->get((int)$region_id);
+
+    $region->delete();
+
+    header('Location: /regions');
+    die;
+});
+
+$app->get('/regions/:region_id', function($region_id) {
+
+    User::verifyLogin();
+
+    $region = new Region();
+
+    $region->get($region_id);
+
+    $page = new Page();
+
+    $page->setTpl('regions-update', array(
+        'region'    => $region->getValues(),
+        'msgError'  => Region::getError()
+    ));
+});
+
+$app->post('/regions/create', function() {
+
+    User::verifyLogin();
+
+    $region = new Region();
+
+    $region->setValues($_POST);
+
+    $region->insert();
+
+    header('Location: /regions');
+    die;
+});
+
+$app->post('/regions/:region_id', function($region_id) {
+
+    User::verifyLogin();
+
+    $region = new Region();
+
+    $region->get((int)$region_id);
+
+    $region->setValues($_POST);
+
+    $region->update();
+
+    header('Location: /regions');
+    die;
+});
+
+?>
