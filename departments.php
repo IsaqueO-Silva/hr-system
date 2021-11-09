d24d5776a8644a52349f79f38684e81b68808a34
Isaque O. Silva <Isaque1945@hotmail.com>
2021-11-08 21:16:09 -0300

    departments.php & Department class

---
+++ departments.php
@@ -0,0 +1,96 @@
+<?php
+
+use \Isaque\Page;
+use \Isaque\Model\User;
+use \Isaque\Model\Department;
+
+$app->get('/departments/search/:department_id', function($department_id) {
+
+    User::verifyLogin();
+
+    echo json_encode(Department::search($department_id));
+});
+
+$app->get('/departments', function() {
+
+    User::verifyLogin();
+
+    $page = new Page();
+
+    $page->setTpl('departments', array(
+        'departments' => Department::listAll(),
+        'msgError'  => Department::getError()
+    ));
+});
+
+$app->get('/departments/create', function() {
+
+    User::verifyLogin();
+
+    $page = new Page();
+
+    $page->setTpl('departments-create', array(
+        'msgError'  => Department::getError()
+    ));
+});
+
+$app->get('/departments/:department_id/delete', function($department_id) {
+
+    User::verifyLogin();
+
+    $department = new Department();
+
+    $department->get((int)$department_id);
+
+    $department->delete();
+
+    header('Location: /departments');
+    die;
+});
+
+$app->get('/departments/:department_id', function($department_id) {
+
+    User::verifyLogin();
+
+    $department = new Department();
+
+    $department->get($department_id);
+
+    $page = new Page();
+
+    $page->setTpl('departments-update', array(
+        'department'   => $department->getValues(),
+        'msgError'  => Department::getError()
+    ));
+});
+
+$app->post('/departments/create', function() {
+
+    User::verifyLogin();
+
+    $department = new Department();
+
+    $department->setValues($_POST);
+
+    $department->insert();
+
+    header('Location: /departments');
+    die;
+});
+
+$app->post('/departments/:department_id', function($department_id) {
+
+    User::verifyLogin();
+
+    $department = new Department();
+
+    $department->get((int)$department_id);
+
+    $department->setValues($_POST);
+
+    $department->update();
+
+    header('Location: /departments');
+    die;
+});
+?>
---
+++ vendor/isaque/php-classes/src/Model/Department.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Isaque\Model;
+
+use Isaque\Model;
+use Isaque\DB\Sql;
+
+class Department extends Model {
+
+    const ERROR = 'DepartmentError';
+
+    public function get($department_id) : void {
+        try {
+
+            $sql = new Sql();
+
+            $values = $sql->select('SELECT * FROM departments a INNER JOIN regions b ON (a.region_id = b.region_id) WHERE(department_id = :department_id);', array(
+                ':department_id'   => $department_id
+            ));
+
+            $this->setValues($values[0]);
+        }
+        catch(\Exception $e) {
+
+            Department::setError('Error capturing the department!');
+            header('Location: /departments');
+            die;
+        }
+    }
+
+    public static function listAll() : array {
+
+        $sql = new Sql();
+
+        return $sql->select('SELECT * FROM departments a INNER JOIN regions b ON (a.region_id = b.region_id) ORDER BY department_name;');
+    }
+
+    public function insert() : void {
+        try {
+
+            if(
+                empty($this->getdepartment_name()) ||
+                empty($this->getregion_id())
+            ) {
+
+                Department::setError('Please fill in all fields!');
+                header('Location: /departments/create');
+                exit;
+            }
+            else {
+                $sql = new Sql();
+
+                $results = $sql->select('CALL sp_departments_save(:pdepartment_id, :pdepartment_name, :pregion_id);', array(
+                    ':pdepartment_id'          => 0,
+                    ':pdepartment_name'        => $this->getdepartment_name(),
+                    ':pregion_id'           => $this->getregion_id()
+                ));
+
+                $this->setValues($results[0]);
+            }
+        }
+        catch(\Exception $e) {
+
+            Department::setError('Error registering the department!');
+            header('Location: /departments/create');
+            die;
+        } 
+    }
+
+    public function update() : void {
+        try {
+
+            if(
+                empty($this->getdepartment_id()) ||
+                empty($this->getdepartment_name()) ||
+                empty($this->getregion_id())
+            ) {
+
+                Department::setError('Please fill in all fields!');
+                header('Location: /departments/'.$this->getdepartment_id());
+                exit;
+            }
+            else {
+
+                $sql = new Sql();
+
+                $results = $sql->select('CALL sp_departments_save(:pdepartment_id, :pdepartment_name, :pregion_id);', array(
+                    ':pdepartment_id'      => $this->getdepartment_id(),
+                    ':pdepartment_name'    => $this->getdepartment_name(),
+                    ':pregion_id'       => $this->getregion_id()
+                ));
+
+                $this->setValues($results[0]);
+            }
+        }
+        catch(\Exception $e) {
+
+            Department::setError('Error updating the department!');
+            header('Location: /departments/'.$this->getdepartment_id());
+            die;
+        } 
+    }
+
+    public function delete() : void {
+        try {
+
+            $sql = new Sql();
+
+            $sql->query('DELETE FROM departments WHERE(department_id = :department_id);', array(
+                ':department_id'    => $this->getdepartment_id()
+            ));
+        }
+        catch(\Exception $e) {
+
+            Department::setError('Error deleting the department!');
+            header('Location: /departments');
+            die;
+        }
+    }
+
+    public static function search($department_id) : array {
+
+        if(!(is_numeric($department_id))) {
+
+            return array();
+        }
+
+        $sql = new Sql();
+
+        return $sql->select('SELECT * FROM departments WHERE(department_id = :department_id);', array(
+            ':department_id'   => $department_id
+        ));
+    }
+
+    public static function setError($msg) : void {
+
+        $_SESSION[Department::ERROR]   = $msg;
+    }
+
+    public static function getError() : string {
+
+        $msg = (isset($_SESSION[Department::ERROR]) && ($_SESSION[Department::ERROR])) ? $_SESSION[Department::ERROR] : '';
+
+        Department::clearError();
+
+        return $msg;
+    }
+
+    public static function clearError() : void {
+
+        $_SESSION[Department::ERROR]    = NULL;
+    }
+}
+?>
