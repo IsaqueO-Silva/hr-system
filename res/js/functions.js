38dc4444d699674098d954523075df6f4ac1b646
Isaque O. Silva <Isaque1945@hotmail.com>
2021-11-10 13:21:14 -0300

    Added the search method(js) in the countries, locations and employees  templates

--- res/js/functions.js
+++ res/js/functions.js
@@ -1,35 +1,3 @@
-function searchCountry(countryId) {
-
-    if(countryId == '') {
-
-        return;
-    }
-    else {
-
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function() {
-
-            if ((this.readyState == 4) && (this.status == 200)) {
-
-                let result = JSON.parse(this.responseText);
-
-                if(result.length == 0) {
-
-                    document.getElementById('country_id').value      = '';
-                    document.getElementById('country_name').value    = '';
-                    return;
-                }
-
-                document.getElementById('country_name').value = result[0].country_name;
-            }
-        }
-
-        xhttp.open('GET', 'http://www.hrsystem.com/countries/search/'+countryId, true);
-        xhttp.send();   
-    }
-}
-
 function searchLocation(locationId) {
 
     if(locationId == '') {
@@ -83,6 +51,11 @@ function search(argId, argModule) {
 
                 if(result.length == 0) {
 
+                    if(module == 'countries') {
+
+                        module = 'country';
+                    }
+
                     document.getElementById(module+'_id').value      = '';
                     document.getElementById(module+'_name').value    = '';
                     return;
@@ -90,15 +63,19 @@ function search(argId, argModule) {
 
                 switch(module) {
 
-                    case 'region':
+                    case 'regions':
                         document.getElementById('region_name').value = result[0].region_name;
                     break;
 
-                    case 'job':
+                    case 'countries':
+                        document.getElementById('country_name').value = result[0].country_name;
+                    break
+
+                    case 'jobs':
                         document.getElementById('job_name').value = result[0].job_title;
                     break;
 
-                    case 'department':
+                    case 'departments':
                         document.getElementById('department_name').value = result[0].department_name;
                     break;
                 }
@@ -106,7 +83,7 @@ function search(argId, argModule) {
             }
         }
 
-        xhttp.open('GET', 'http://www.hrsystem.com/'+module+'s/search/'+id, true);
+        xhttp.open('GET', 'http://www.hrsystem.com/'+module+'/search/'+id, true);
         xhttp.send();   
     }
 }
