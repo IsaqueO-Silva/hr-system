b96a983df1602f3185b5acae29c49c198002753d
Isaque O. Silva <Isaque1945@hotmail.com>
2021-11-15 16:08:33 -0300

    Adjustments in forgot.html

--- views-cache/forgot.05056d9b28ceb70793c7dfdfc060271b.rtpl.php
+++ views-cache/forgot.05056d9b28ceb70793c7dfdfc060271b.rtpl.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta charset="utf-8"/>
     <title>Hr SYSTEM</title>
     <style>
