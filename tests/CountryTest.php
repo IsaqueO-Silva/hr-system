dcd757765c8b4a7e0c46346737d6872926d24f37
Isaque O. Silva <Isaque1945@hotmail.com>
2021-12-28 21:44:31 -0300

    Class CountryTest.php

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -1,8 +1,38 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Isaque\Model\Country;
 
 class CountryTest extends TestCase {
-    
+   
+    public function testGetCountryFromDatabase() {
+
+        $country = new Country();
+
+        $country->get('1');
+
+        $expected = array(
+            'country_id'    => '1',
+            'country_name'  => 'Brazil',
+            'region_id'     => '1',
+            'region_name'   => 'South America'
+        );
+
+        $this->assertEquals($expected, $country->getValues());
+    }
+
+    public function testListAllCountriesFromDatabase() {
+
+        $expected = array(
+            array(
+                'country_id'    => '1',
+                'country_name'  => 'Brazil',
+                'region_id'     => '1',
+                'region_name'   => 'South America'
+            )
+        );
+
+        $this->assertEquals($expected, Country::listAll());
+    }
 }
 ?>
