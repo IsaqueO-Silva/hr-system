a7b6f7ac3b5bebdcd261832aca2f0732e5ac58e5
Isaque O. Silva <Isaque1945@hotmail.com>
2021-12-29 21:10:37 -0300

    Created some tests in CountryTest class

--- tests/CountryTest.php
+++ tests/CountryTest.php
@@ -4,8 +4,8 @@ use PHPUnit\Framework\TestCase;
 use Isaque\Model\Country;
 
 class CountryTest extends TestCase {
-   
-    public function testGetCountryFromDatabase() {
+
+    public function testGetACountryFromDatabase() {
 
         $country = new Country();
 
@@ -33,6 +33,93 @@ class CountryTest extends TestCase {
         );
 
         $this->assertEquals($expected, Country::listAll());
+    }
+
+    public function testInsertACountryInDatabase() {
+
+        $country = new Country();
+
+        $country->setValues(array(
+            'country_name'  => 'Chile',
+            'region_id'     => '1'
+        ));
+
+        $country->insert();
+
+        $expected = array(
+            'country_id'    => $country->getcountry_id(),
+            'country_name'  => $country->getcountry_name(),
+            'region_id'     => $country->getregion_id()
+        );
+
+        $this->assertEquals($expected, $country->getValues());
+    }
+
+    public function testUpdateACountryInDatabase() {
+
+        $country = new Country();
+
+        $country->setValues(array(
+            'country_id'    => '7',
+            'country_name'  => 'Guyana',
+            'region_id'     => '1'
+        ));
+
+        $country->update();
+
+        $expected = array(
+            'country_id'    => $country->getcountry_id(),
+            'country_name'  => $country->getcountry_name(),
+            'region_id'     => $country->getregion_id()
+        );
+
+        $this->assertEquals($expected, $country->getValues());
+    }
+
+    public function testDeleteACountryInDatabase() {
+
+        $country = new Country();
+
+        $country->setValues(array(
+            'country_id'    => '7'
+        ));
+
+        $expected = '';
+
+        $this->assertEquals($expected, $country->delete());
+    }
+
+    public function testSearchACountryInDatabase() {
+
+        $expected = array(
+            array(
+                'country_id'      =>    '1',
+                'country_name'    =>    'Brazil',
+                'region_id'       =>    '1'
+            )
+        );
+
+        $this->assertEquals($expected, Country::search(1));
+    }
+
+    public function testSetAndGetAnErrorMessageForTheCountryClass() {
+
+        $expected = 'Error message';
+
+        Country::setError('Error message');
+
+        $this->assertEquals($expected, Country::getError());
+    }
+
+    public function testClearAnErrorMessageFromTheCountryClass() {
+
+        $expected = NULL;
+
+        Country::setError('Error message');
+
+        Country::clearError();
+
+        $this->assertEquals($expected, Country::getError());
     }
 }
 ?>
